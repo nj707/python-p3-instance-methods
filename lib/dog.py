7936b31417d7e1a93c442c8642c654c0e1ef6481
Nial Johnson <nialj707@gmail.com>
2023-09-17 20:22:30 -0700

    Complete

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,14 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+
+fido = Dog()
+fido.bark()
+fido.sit()
--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,14 @@
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+Nial = Person()
+Nial.walk()
+Nial.talk()
